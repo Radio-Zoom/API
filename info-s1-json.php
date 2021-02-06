3c1799d7b581fe8e9cbdfdda57f83079d2f07b17
Malte Schroeder <post@malte-schroeder.de>
2021-02-06 11:09:16 +0100

    Add missing file for mAirlist-Control

---
+++ info-s1-json.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Get JSON Data from your Icecast2 Server to be used with mairlist-control
+
+ *
+ * @package     
+ * @license     http://www.gnu.org/licenses/agpl.html AGPL Version 3
+ * @author      Malte Schroeder <post@malte-schroeder.de>
+ * @copyright   Copyright (c) 2017-2021 Malte Schroeder (http://www.malte-schroeder.de)
+ *
+ */
+
+header('Access-Control-Allow-Origin: *');
+require('function.inc.php');
+require('config.inc.php');
+
+$s1 = getAdminInfo($server1, $adminpass);
+
+echo json_encode($s1, JSON_FORCE_OBJECT);
+#print_r ($s1);
+?>
